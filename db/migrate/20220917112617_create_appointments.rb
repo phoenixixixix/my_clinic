9c510ed54b8147b884a8c77e6d5421cd924e6444
phoenixixixix <barchukoleg12@gmail.com>
2022-09-17 17:41:12 +0300

    Setup relation between Doctor and Patient through Appointment:
    * Create model Doctor;
    * Create model Patient;
    * Create Appointments table and model;
    * Setup associations Doctor and Patient throught join model/table Appointment;

---
+++ db/migrate/20220917112617_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.belongs_to :doctor
+      t.belongs_to :patient
+
+      t.timestamps
+    end
+  end
+end
