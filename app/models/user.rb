1b6b5f7203cbb990899e1e7d224a0a1e2dce3661
phoenixixixix <barchukoleg12@gmail.com>
2022-09-18 16:43:57 +0300

    Add User methods to determine Type of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def doctor?
+    type == "Doctor"
+  end
+  def patient?
+    type == "Patient"
+  end
 end
